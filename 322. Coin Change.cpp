72d7f4586511a38f872a75763e4d9b68de0d98f4
Will Gaston <wsegaston@gmail.com>
2025-11-17 12:19:55 +1100

    Create 322. Coin Change.cpp

---
+++ 322. Coin Change.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // let dp[i] be the smallest number of coins needed to reach amount i
+        vector<int> dp(amount + 1, INT_MAX);
+
+        for (int c : coins) {
+            if (c < amount) dp[c] = 1;
+        }
+
+        dp[0] = 0;
+
+        for (int i = 0; i <= amount; i++) {
+            for (int c : coins) {
+                if (i - c >= 0 && dp[i - c] < INT_MAX) dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        }
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
